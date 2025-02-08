25c9b5022e1915fbf1e9728bd5012ec0a816a120
LeeEzra <ronoezra32@gmail.com>
2025-02-08 18:30:36 +0300

    update

--- src/general_components/Register.js
+++ src/general_components/Register.js
@@ -5,9 +5,10 @@ import loaderIcon from '../images/icons/settings.png';
 
 function Register() {
   const [firstName, setFirstName] = useState('');
+  const [middleName, setMiddleName] = useState('');
   const [lastName, setLastName] = useState('');
   const [gender, setGender] = useState('');
-  const [idNumber, setIdNumber] = useState('');
+  const [country, setCountry] = useState('');
   const [email, setEmail] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [password, setPassword] = useState('');
@@ -47,7 +48,8 @@ function Register() {
         firstName,
         lastName,
         gender,
-        idNumber
+        country,
+        middleName
       });
       navigate('/login');
     } catch (error) {
@@ -70,6 +72,12 @@ function Register() {
         required
       />
       <input
+        type="text" name='middlename' id='middlename'
+        value={middleName}
+        onChange={(e) => setMiddleName(e.target.value)}
+        placeholder="Middle Name(Optional)"
+      />
+      <input
         type="text" name='lastname' id='lastname'
         value={lastName}
         onChange={(e) => setLastName(e.target.value)}
@@ -84,16 +92,61 @@ function Register() {
           <option value='' disabled>Gender</option>
           <option value='male'>Male</option>
           <option value='female'>Female</option>
-          <option value='other'>Other</option>
         </select>
-      <input
-        type="text" name='idnumber' id='idnumber'
-        value={idNumber}
-        onChange={(e) => setIdNumber(e.target.value)}
-        placeholder="Id Number"
-        autoComplete='off'
-        required
-      />
+        <select type="text" name='country' id='country' value={country} onChange={(e) => setCountry(e.target.value)} required>
+          <option value="" disabled selected>Select Country</option>
+          <option value="algeria">Algeria</option>
+          <option value="niger">Niger</option>
+          <option value="angola">Angola</option>
+          <option value="south-africa">South Africa</option>
+          <option value="uganda">Uganda</option>
+          <option value="cameroon">Cameroon</option>
+          <option value="botswana">Botswana</option>
+          <option value="eritrea">Eritrea</option>
+          <option value="namibia">Namibia</option>
+          <option value="djibouti">Djibouti</option>
+          <option value="lesotho">Lesotho</option>
+          <option value="eswatini">Eswatini</option>
+          <option value="nigeria">Nigeria</option>
+          <option value="kenya">Kenya</option>
+          <option value="tanzania">Tanzania</option>
+          <option value="ethiopia">Ethiopia</option>
+          <option value="sao-tome-and-principe">São Tomé and Príncipe</option>
+          <option value="comoros">Comoros</option>
+          <option value="cabo-verde">Cabo Verde</option>
+          <option value="mauritius">Mauritius</option>
+          <option value="seychelles">Seychelles</option>
+          <option value="chad">Chad</option>
+          <option value="senegal">Senegal</option>
+          <option value="ghana">Ghana</option>
+          <option value="gabon">Gabon</option>
+          <option value="burkina-faso">Burkina Faso</option>
+          <option value="central-african-republic">Central African Republic</option>
+          <option value="equatorial-guinea">Equatorial Guinea</option>
+          <option value="benin">Benin</option>
+          <option value="guinea">Guinea</option>
+          <option value="guinea-bissau">Guinea-Bissau</option>
+          <option value="liberia">Liberia</option>
+          <option value="madagascar">Madagascar</option>
+          <option value="mali">Mali</option>
+          <option value="mauritania">Mauritania</option>
+          <option value="sierra-leone">Sierra Leone</option>
+          <option value="togo">Togo</option>
+          <option value="democratic-republic-of-the-congo">Democratic Republic of the Congo</option>
+          <option value="cote-divoire">Côte d'Ivoire</option>
+          <option value="the-gambia">The Gambia</option>
+          <option value="south-sudan">South Sudan</option>
+          <option value="burundi">Burundi</option>
+          <option value="malawi">Malawi</option>
+          <option value="mozambique">Mozambique</option>
+          <option value="rwanda">Rwanda</option>
+          <option value="zimbabwe">Zimbabwe</option>
+          <option value="zambia">Zambia</option>
+          <option value="morocco">Morocco</option>
+          <option value="egypt">Egypt</option>
+          <option value="libya">Libya</option>
+          <option value="somalia">Somalia</option>
+        </select>
       <input
         type="text" name='phonenumber' id='phonenumber'
         value={phoneNumber}
